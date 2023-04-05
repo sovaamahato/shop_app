899d885e19ff77eb84b72b1d1c768c7d4953bbf0
sova <mahatoshobha143@gmail.com>
2023-04-05 22:45:36 +0545

    working on cart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/providers/cart.dart';
 import 'package:shop_app/providers/products.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
 import 'package:shop_app/screens/product_overview_screen.dart';
@@ -13,8 +14,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-    create: (ctx) => Products(),
+    return MultiProvider(providers: [
+      ChangeNotifierProvider(
+    create: (ctx) => Products(),),
+    ChangeNotifierProvider(
+    create: (ctx) => Cart(),),
+
+    ], 
       
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
--- lib/widget/product_item.dart
+++ lib/widget/product_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/providers/cart.dart';
 import 'package:shop_app/screens/product_detail_screen.dart';
 
 import '../providers/product.dart';
@@ -13,6 +14,7 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final product= Provider.of<Product>(context,listen: false);
+    final cart= Provider.of<Cart>(context,listen: false);
     return ClipRRect(
         borderRadius: BorderRadius.circular(10),
         child: GridTile(
@@ -45,7 +47,8 @@ class ProductItem extends StatelessWidget {
               icon: Icon(
                 Icons.shopping_cart,
               ),
-              onPressed: () {},
+              onPressed: () {
+                cart.addItem(product.id, product.price, product.title) ;             },
             ),
           ),
         ),
